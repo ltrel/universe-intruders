99dac12442efd30e88484d6edccdb86ad04e4129
ltrel <79512656+ltrel@users.noreply.github.com>
2020-10-14 17:18:52 +1100

    Add resources class with player texture

---
+++ Resources.cs
@@ -0,0 +1,12 @@
+// Resources.cs
+// Created on: 2020-10-14
+// Author: Leo Treloar
+
+using System;
+using SFML.Graphics;
+
+namespace UniverseIntruders {
+    static class Resources {
+        public static Texture Player = new Texture("Resources\\player.png");
+    }
+}
