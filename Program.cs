39c4097f64a01597ab846063c431e3f00a5c1239
ltrel <79512656+ltrel@users.noreply.github.com>
2020-10-14 12:18:34 +1100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UniverseIntruders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
