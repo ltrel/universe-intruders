b44f16d12842d95af042c0ce53e635bc2cf1bd40
ltrel <79512656+ltrel@users.noreply.github.com>
2020-10-23 17:02:55 +1100

    Add comments explaining random movement

--- Entities/EnemyRandom.cs
+++ Entities/EnemyRandom.cs
@@ -24,16 +24,19 @@ namespace UniverseIntruders
         protected override void SetNextDestination()
         {
             Vector2f newDestination;
-
             do
             {
+                // Get a random angle and random distance between the minimum and maximum
                 double angle = Game.Rand.NextDouble() * 360;
                 float distance = (float)Game.Rand.NextDouble() * (MaxDistance - MinDistance) + MinDistance;
+                // Convert the angle and distance to cartesian coordinates
                 Vector2f step = new Vector2f(
                     distance * (float)Math.Cos(angle),
                     distance * (float)Math.Sin(angle)
                 );
+                // Add this onto the enemy's current position
                 newDestination = Position + step;
+                // If the destination is outside of the bounds, try again
             } while (!Collision.IsPointInRect(newDestination, movementBounds));
             currentDestination = newDestination;
         }
