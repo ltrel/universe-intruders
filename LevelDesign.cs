9e1d9d22a396b15709e0e7a08e31b365daeacab9
ltrel <79512656+ltrel@users.noreply.github.com>
2020-10-26 15:14:02 +1100

    Remove unnecessary lines setting font color to white

--- LevelDesign.cs
+++ LevelDesign.cs
@@ -63,25 +63,21 @@ namespace UniverseIntruders
             background.Depth = 1;
             background.Initialize();
             Text title = new Text("Universe Intruders", Resources.Fonts["ibmbios"], 50);
-            title.FillColor = Color.White;
             title.Position = new Vector2f(
                 window.DefaultView.Size.X / 2 - title.GetLocalBounds().Width / 2,
                 window.DefaultView.Size.Y * 0.16f - title.GetLocalBounds().Height / 2);
             Texts.Add(title);
             Text highScore = new Text("High score: 0000", Resources.Fonts["ibmbios"], 32);
-            highScore.FillColor = Color.White;
             highScore.Position = new Vector2f(
                 window.DefaultView.Size.X / 2 - highScore.GetLocalBounds().Width / 2,
                 window.DefaultView.Size.Y * 0.5f - highScore.GetLocalBounds().Height / 2);
             Texts.Add(highScore);
             Text play = new Text("Press SPACE to start", Resources.Fonts["ibmbios"], 32);
-            play.FillColor = Color.White;
             play.Position = new Vector2f(
                 window.DefaultView.Size.X / 2 - play.GetLocalBounds().Width / 2,
                 window.DefaultView.Size.Y * 0.5f - play.GetLocalBounds().Height / 2 + play.CharacterSize * 3);
             Texts.Add(play);
             Text quit = new Text("Or press Q to quit", Resources.Fonts["ibmbios"], 32);
-            quit.FillColor = Color.White;
             quit.Position = new Vector2f(
                 window.DefaultView.Size.X / 2 - quit.GetLocalBounds().Width / 2,
                 window.DefaultView.Size.Y * 0.5f - quit.GetLocalBounds().Height / 2 + quit.CharacterSize * 6);
